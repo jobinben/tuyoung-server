4bdfa0877bd007a07e78bb5728947f2b4cf30ae9
jobinben <1021137079@qq.com>
2021-09-04 15:34:33 +0800

    官网部署了tuyoung-ui和ejs和vue

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const ejs = require('ejs')
+
+
+// 引用引擎
+app.engine('html', ejs.__express)
+
+//设置引擎
+app.set('view engine', 'html')
 
 //配置静态目录
 app.use(express.static(path.join(__dirname, 'node_modules/tuyoung/dist')))
 
 app.get('/', (req, res) => {
-    res.send('欢迎来到大冰的资源网')
+    res.render('index.html')
 })
+
+
 
 app.listen(1114, () => {
     console.log('127.0.0.1:1114')
